63a05edc7e2521058c8160897db19d68085e7d3c
407 <407@academicos.cesde.edu.co>
2022-11-22 16:05:36 -0500

    ok

---
+++ src/controllers/listadoPeliculas.js
@@ -0,0 +1,226 @@
+let peliculas=[
+    
+    {
+        nombre:"Hallowen la noche final",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/hallowen%20ultimo%20poster.jpg?alt=media&token=4cc9a9c8-c7bb-4462-9675-2dd057e889f0",
+        sinopsis:"Cuatro años después de la desaparición de Michael Myers, el joven Corey lo encuentra maltrecho en las alcantarillas. Poco a poco, se establece un extraño vínculo entre estos dos personajes unidos por la venganza y la locura.",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/halloween-kills.jpeg?alt=media&token=7378da01-2dca-4fd4-85ca-98387c9166a9",
+        genero:"Terror"
+    },
+    {
+        nombre:"La noche del demonio",
+        duracion:90,
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/jeeppers.jpg?alt=media&token=c6c07002-c476-4095-900f-011047e967dc",
+        sinopsis:"Josh (Patrick Wilson), su esposa Renai (Rose Byrne) y sus tres hijos acaban de mudarse a una vieja casa. Pero, tras un desgraciado accidente, uno de los niños entra en coma y, al mismo tiempo, empiezan a producirse en la casa extraños fenómenos que aterrorizan a la familia.",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemajaider.appspot.com/o/nochedemonio.jpg?alt=media&token=d7e10b9f-e118-4dba-86a9-012b0220b7a7",
+        genero:"Terror"
+    },
+    {
+        nombre:"Minions",
+        duracion:85,
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/minions2posternuevo.jpg?alt=media&token=dc7c97ca-ebb7-4342-b0c9-a9fd7b0f1af1",
+        sinopsis:"En los años 70, Gru crece siendo un gran admirador de Los salvajes seis, un supergrupo de villanos. Para demostrarles que puede ser malvado, Gru idea un plan con la esperanza de formar parte de la banda. Por suerte, cuenta con la ayuda de sus fieles seguidores, los Minions, siempre dispuestos a sembrar el caos.",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemajaider.appspot.com/o/minions.webp?alt=media&token=0147e52d-cd36-4b8b-8d86-4fe21db3cce9",
+        genero:"Animada"
+    },
+    {
+        nombre:"Tadeo",
+        duracion:104,
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/tadeo3nuevoposter.jpg?alt=media&token=3b31d4c0-3c0b-4317-ba8a-fc47a47e2a84",
+        sinopsis:"Tadeo, un albañil que sueña con ser arqueólogo, es enviado por error a una expedición en Perú. Con la ayuda de su fiel perro Jeff, una intrépida profesora, un loro mudo y un buscavidas, intentará salvar la mítica ciudad perdida de los Incas de una malvada empresa de cazatesoros.",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemajaider.appspot.com/o/tadeo-jones-3.webp?alt=media&token=5b041a7e-af1a-457e-abad-e8350a24452e",
+        genero:"Animada"
+    },
+    {
+        nombre: "Wakanda forever",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690",
+        sinopsis:"Una secuela que seguirá explorando el incomparable mundo de Wakanda y todos los ricos y variados personajes presentados en la película de 2018",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemajaider.appspot.com/o/wakanda-forever.webp?alt=media&token=1e6033e1-3f7c-4294-8edd-24f1442477e8",
+        genero:"Ciencia ficcion"
+    },
+    {
+        nombre: "CATOCLISMO",
+        duracion:115,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/EL%20CATACLISMO1.jpg?alt=media&token=ca9202fb-d5ff-4261-87f8-2592a4dcafd0",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/cato.jpg?alt=media&token=70f36cbc-dd39-4931-ad2e-7e0b33fc826c",
+        genero:"Drama"
+    },
+    {
+        nombre: "titanic",
+        duracion:85,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/titanic.jpg?alt=media&token=889358e5-c867-4249-8934-f9aa789b7f1b",
+        sinopsis:"The Woman es la última superviviente del violento clan que ha estado en la costa noreste durante décadas. Severamente herida y vulnerable, la mujer se convierte en una presa fácil para el perturbado abogado Christopher Cleek, el cual se embarcará en un desquiciado proyecto para capturarla y hacerla pedazos, una decisión que no tardará en amenazar su vida y la de los suyos",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/titanic1.jpg?alt=media&token=3d47baaf-0e37-4964-8463-1f9d5c24b4a2",
+        genero:"Drama"
+    },
+    {
+        nombre: "sonrie1",
+        duracion:135,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemajaider.appspot.com/o/SONRIE1.jpg?alt=media&token=759d87fa-6b61-4ecb-aa85-4060fda90fdd",
+        sinopsis:"Tras presencia el dramático incidente sufrido por un paciente, la Dra. Cotter empieza a experimentar hechos aterradores sin explicación aparente. A medida que el horror se adueña de su vida, comprende que la respuesta está en su propio pasado.",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemajaider.appspot.com/o/noticia-Sonrie.jpg?alt=media&token=a8b35fd3-f488-4d20-b542-74466a05a30a",
+        genero:"Terror"
+    },
+    {
+        nombre: "el coco",
+        duracion:105,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/el%20coco.jpg?alt=media&token=995b11bb-5106-49d0-ac62-0c6415627d4e",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/el%20coco%201.png?alt=media&token=ac383c3e-c246-4ddb-a533-7275425ba626",
+        genero:"Aventura y Drama"
+    },
+    {
+        nombre: "trolls",
+        duracion:90,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/t.jpg?alt=media&token=30d83297-0dba-4fc3-9eff-d4395a8de98d",
+        sinopsis:"En un centro de rehabilitación experimental en medio de la selva, Eliú paga una sentencia por asesinato. Cuando su mejor amigo y cómplice es trasladado al mismo lugar, los jóvenes deberán reconstruir su crimen y enfrentar un pasado del que Eliú se quiere alejar. En medio de terapias y trabajos forzados, Eliú se enfrentará a la oscuridad de la naturaleza humana y tratará de escapar de la suya antes de que sea demasiado tarde.",
+        poster2:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/troo.png?alt=media&token=69eab8ad-3cbb-4960-a096-d15b64e56057",
+        genero:"Drama"
+    }
+]
+
+let peliculasproximoExtreno=[
+ 
+
+        {
+    
+            nombre: "mision imposible",
+                duracion:120,
+                poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/mision.jpg?alt=media&token=a7850289-1e23-4c52-bd9c-30378d0fb1cd",
+                genero:"accion"
+            },
+            {
+        
+                nombre: "maria broos",
+                    duracion:180,
+                    poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/mario%20bros.jpg?alt=media&token=f8004ac6-1995-454b-896e-64f99feb0090",
+                    genero:"animada"
+                },
+                {
+                nombre: "avatar",
+                duracion:120,
+                poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/avatar%20estreno%202.jpg?alt=media&token=4dbccd41-6063-4aa8-a937-cca390debe6a",
+                genero:"Ciencia ficción"
+            },
+            {
+                nombre: "gato con botas",
+                duracion:140,
+                poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/gatos%20con%20botas.jpg?alt=media&token=ebdfe27c-f375-4731-8144-3b5ae310da0f",
+                genero:"animada"
+            },
+            {
+                nombre: "aguaman",
+                duracion:130,
+                poster:"https://firebasestorage.googleapis.com/v0/b/cinemakarina.appspot.com/o/agua%20man.jpg?alt=media&token=a47a9cf8-9c78-49f9-8258-f7a88af58013",
+                genero:"Ciencia ficción"
+            }
+    ]
+//creamos una referencia al espacio html donde queremos hacer el render(pintar etiquetas)
+let etiquetafila=document.getElementById("fila")
+
+let etiquetafilaExtreno=document.getElementById("filaExtreno")
+
+/*si ya tengo datos que normalmente llegan al front 
+como un arreglo de objetos debemos recorrer y depurar
+dicha informacion */
+
+
+//para pintar la informacion de cada pelicula debemos 
+//aplicar una tecnica conocida como traversing
+//traversing=crear etiquetas de html desde js
+
+peliculasproximoExtreno.forEach(function(peliculaproximoExtreno){
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    let poster=document.createElement("img")
+    poster.classList.add("img-fluid","w-100")
+    poster.src=peliculaproximoExtreno.poster
+
+    let nombre=document.createElement("h3")
+    nombre.classList.add("text-center", "fw-bold")
+    nombre.textContent=peliculaproximoExtreno.nombre
+
+    let duraccion=document.createElement("h4")
+    duraccion.classList.add("text-start")
+    duraccion.textContent="duracion: "+peliculaproximoExtreno.duraccion+"min"
+
+    let genero=document.createElement("h5")
+    genero.classList.add("text-start")
+    genero.textContent="Genero: "+peliculaproximoExtreno.genero
+
+    etiquetafilaExtreno.appendChild(columna)
+    columna.appendChild(tarjeta)
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(duraccion)
+})
+
+peliculas.forEach(function(pelicula){
+
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    let poster=document.createElement("img")
+    poster.classList.add("img-fluid","w-100")
+    poster.src=pelicula.poster
+
+    let nombre=document.createElement("h3")
+    nombre.classList.add("text-center","fw-bold")
+    nombre.textContent=pelicula.nombre
+
+    let duracion=document.createElement("h4")
+    duracion.classList.add("text-start")
+    duracion.textContent="duracion: "+pelicula.duracion+"min"
+
+    let sinopsis=document.createElement("p")
+    sinopsis.classList.add("d-none")
+    sinopsis.textContent=pelicula.sinopsis
+
+    let poster2=document.createElement("video")
+    poster2.classList.add("d-none")
+    poster2.src=pelicula.poster2
+
+    let genero=document.createElement("h5")
+    genero.classList.add("text-start")
+    genero.textContent="Genero: "+pelicula.genero
+
+    //jerarquia de etiquetas(padres e hijos)
+    etiquetafila.appendChild(columna)
+    columna.appendChild(tarjeta)
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(poster2)
+    tarjeta.appendChild(duracion)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(sinopsis)
+})
+
+//Escuchadno el evento del clic en alguna pelicula de la cartelera
+
+let peliculaSelecionada={}
+
+etiquetafila.addEventListener("click", function(evento){
+   if(evento.target.classList.contains("img-fluid")){
+   // window.location.href="./src/views/verinfopelicula.html"
+   // obteniendo los datos de la pelicula 
+   // para ingresar a la fuente de una etiqueta se usa el src si es imagen o texconten si es texto
+   peliculaSelecionada.imagen=evento.target.parentElement.querySelector('img').src
+   peliculaSelecionada.titulo=evento.target.parentElement.querySelector('h3').textContent
+   peliculaSelecionada.duraccion=evento.target.parentElement.querySelector('h4').textContent
+   peliculaSelecionada.sinopsis=evento.target.parentElement.querySelector("P").textContent
+   peliculaSelecionada.imagendos=evento.target.parentElement.querySelector('video').src
+    
+   //enviando nuestros datos de la pelicula selecionada a la memoria
+   localStorage.setItem("pelicula", JSON.stringify(peliculaSelecionada))
+   window.location.href="./src/views/verinfopelicula.html"
+   }
+})
